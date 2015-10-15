979c1d8ea04cd7a9571409e3ebd49e8ca0e653e8
cpp5126 <cpp5126@OZWDLND341-71.win.psu.edu>
2015-10-15 09:02:34 -0400

    Added getId method in controller

--- PopulateList/src/populatelist/FXMLDocumentController.java
+++ PopulateList/src/populatelist/FXMLDocumentController.java
@@ -28,73 +28,75 @@ import javax.persistence.Persistence;
  */
 public class FXMLDocumentController implements Initializable {
 
-  @FXML
-  private Label label;
-  @FXML
-  private Button button;
-  @FXML
-  private ListView<String> lvPeople;
-  @FXML
-  private TextField tfFirstName;
-  @FXML
-  private TextField tfLastName;
-  @FXML
-  private Button btnAddPerson;
+    @FXML
+    private Label label;
+    @FXML
+    private Button button;
+    @FXML
+    private ListView<String> lvPeople;
+    @FXML
+    private TextField tfFirstName;
+    @FXML
+    private TextField tfLastName;
+    @FXML
+    private Button btnAddPerson;
     @FXML
     private Button btnDelete;
 
-  @FXML
-  private void handleButtonAction(ActionEvent event) {
-    System.out.println("You clicked me!");
-    label.setText("Hello World!");
-  }
-
-  @Override
-  public void initialize(URL url, ResourceBundle rb) {
-    // TODO
-    SyncPeopleListView();
-  }
-
-
-  public void SyncPeopleListView() {
-
-    // Items inside the list
-    ObservableList<String> items = lvPeople.getItems();
-
-    // Clear out the list
-    items.clear();
-
-    // Get list of all people in the database
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("PopulateListPU");
-    PersonJpaController jpaPerson = new PersonJpaController(emf);
-    List<Person> people = jpaPerson.findPersonEntities();
-
-    // Add each person to the list
-    for (Person p : people) {
-      lvPeople.getItems().add(p.getFirstName() + " " + p.getLastName());
-    }
-  }
-
-  @FXML
-  private void handleBtnAddPersonClicked(MouseEvent event) {
-    Person newPerson = new Person();
-    newPerson.setFirstName(tfFirstName.getText());
-    newPerson.setLastName(tfLastName.getText());
-
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("PopulateListPU");
-    PersonJpaController jpaPerson = new PersonJpaController(emf);
-
-    try {
-      jpaPerson.create(newPerson);
-    } catch (Exception ex) {
-      Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
     }
 
-    SyncPeopleListView();
-  }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        SyncPeopleListView();
+    }
+
+    public void SyncPeopleListView() {
+
+        // Items inside the list
+        ObservableList<String> items = lvPeople.getItems();
+
+        // Clear out the list
+        items.clear();
+
+        // Get list of all people in the database
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PopulateListPU");
+        PersonJpaController jpaPerson = new PersonJpaController(emf);
+        List<Person> people = jpaPerson.findPersonEntities();
+
+        // Add each person to the list
+        for (Person p : people) {
+            lvPeople.getItems().add(p.getId() + " , " + p.getFirstName() + " " + p.getLastName());
+        }
+    }
+
+    @FXML
+    private void handleBtnAddPersonClicked(MouseEvent event) {
+        Person newPerson = new Person();
+        newPerson.setFirstName(tfFirstName.getText());
+        newPerson.setLastName(tfLastName.getText());
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PopulateListPU");
+        PersonJpaController jpaPerson = new PersonJpaController(emf);
+
+        try {
+            jpaPerson.create(newPerson);
+        } catch (Exception ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        SyncPeopleListView();
+    }
 
     @FXML
     private void handleBtnDeletePersonClicked(MouseEvent event) {
+        String selectedText = lvPeople.getSelectionModel().getSelectedItem();
+        System.out.println("Delete " + selectedText);
+
     }
 
 }
